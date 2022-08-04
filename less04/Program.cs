81871002187bb4f29482d318cabcbfd9aa44bf95
Igor Chichinin <marafon4k@gmail.com>
2022-08-05 00:42:06 +0300

    Добавил решение задачи 25

--- less04/Program.cs
+++ less04/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+
+// 3, 5 -> 243 (3⁵)
+
+// 2, 4 -> 16
+
+Console.WriteLine("Введите число: ");
+int number_1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число, в степень которую возвести: ");
+int number_2 = Convert.ToInt32(Console.ReadLine());
+
+int GetDegree(int num_1, int num_2) {
+    int res = num_1;
+    for (int i = 1; i < num_2; i++)
+    {
+        res = num_1 * res;
+    }
+    return res;
+}
+
+Console.WriteLine($"Число {number_1} в степени {number_2} = {GetDegree(number_1, number_2)}");
