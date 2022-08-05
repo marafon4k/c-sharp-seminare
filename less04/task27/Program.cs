4306073d9fd611fe3b0be7fb7b8b7e4e2f6ab9d3
Igor Chichinin <marafon4k@gmail.com>
2022-08-05 20:51:16 +0300

    Added solutions task 27

---
+++ less04/task27/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int GetSum(int x) {
+    int sum = 0;
+
+    for (int i = x; i > 10; i--)
+    {
+        sum += x % 10;
+        x -= x % 10;
+        x /= 10;
+    }
+    return sum;
+}
+
+Console.WriteLine($"Сумма цифр числа - {GetSum(number)}");
