54e49eb5d9d17f28edb5252db3bc7b4fd1f63f44
Igor Chichinin <marafon4k@gmail.com>
2022-08-09 00:24:13 +0300

    Save solutions, have a bug (don't calc max - min, just write max)

--- less05/task38/Program.cs
+++ less05/task38/Program.cs
@@ -1,2 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 38: Задайте массив вещественных чисел. 
+// Найдите разницу между максимальным и минимальным элементов массива.
+
+// [3.5, 7.1, 22.9, 2.3, 78.5] -> 76.2
+
+Console.WriteLine("Введите количетсво элеметов в массив: ");
+int amount = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(GetDiff(SetRndFlt(amount)));
+
+double[] SetRndFlt(int size)
+{
+    double[] arr = new double[size];
+    Random rnd = new Random();
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = Math.Round(rnd.NextDouble() * 100, 2);
+        Console.WriteLine(arr[i]); // Выводил для теста цифр
+    }
+    return arr;
+}
+
+double GetDiff(double[] arr)
+{
+    double min = 0;
+    double max = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] < min) min = arr[i];
+        if (arr[i] > max) max = arr[i];
+    }
+    double sum = max - min;
+    return sum;
+}
