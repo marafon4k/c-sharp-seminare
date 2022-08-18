13b7aac901bfaabaa0db15d1432068223e5ccb2e
Igor Chichinin <marafon4k@gmail.com>
2022-08-18 19:39:19 +0300

    tasks from lession 07

---
+++ less07/task49/Program.cs
@@ -0,0 +1,38 @@
+﻿// Задача 49: Задайте двумерный массив. Найдите элементы, у
+// которых оба индекса чётные, и замените эти элементы на их
+// квадраты.
+
+int[,] GetMatrix(int m, int n)
+{
+    int[,] matrix = new int[m, n];
+    Random rnd = new Random();
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(1, 10);
+            if (i % 2 == 0 && j % 2 == 0)
+            {
+                matrix[i, j] *= matrix[i, j];
+            }
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            if (j == 0) Console.Write("[");
+            if (j < array.GetLength(1) - 1) Console.Write($"{array[i, j],3}, ");
+            else Console.Write($"{array[i, j],3}]");
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] matrixResoult = GetMatrix(3, 4);
+PrintMatrix(matrixResoult);
---
+++ less07/task51/Program.cs
@@ -0,0 +1,68 @@
+﻿// Задача 51: Задайте двумерный массив. Найдите сумму
+// элементов, находящихся на главной диагонали (с индексами
+// (0;0); (1;1) и т.д.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Сумма элементов главной диагонали: 1+9+2 = 12
+
+// Новый массив будет выглядеть вот так:
+// 1 4 7 2
+// 5 81 2 9
+// 8 4 2 4
+
+Console.Clear();
+
+int[,] GetMatrix(int m, int n)
+{
+    int[,] matrix = new int[m, n];
+    Random rnd = new Random();
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(1, 10);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            if (j == 0) Console.Write("[");
+            if (j < array.GetLength(1) - 1) Console.Write($"{array[i, j],3}, ");
+            else Console.Write($"{array[i, j],3}]");
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] matrixResoult = GetMatrix(3, 4);
+PrintMatrix(matrixResoult);
+
+int SumMatrix(int[,] matrix)
+{
+    int sum = 0;
+    int numbers = 0;
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (i == numbers && j == numbers)
+            {
+                sum += matrix[i, j];
+                numbers++;
+            }
+        }
+    }
+    // Console.WriteLine($"Сумма элементов на диагонали: {sum}");
+    return sum;
+}
+
+int matrixSum = SumMatrix(matrixResoult);
+Console.WriteLine($"Сумма элементов на диагонали: {matrixSum}");
