5e4dbf790ef4694101d088388da86dac0f29261c
Igor Chichinin <marafon4k@gmail.com>
2022-08-20 16:11:17 +0300

    Added solutions tasks from less live

--- less08/task55/Program.cs
+++ less08/task55/Program.cs
@@ -3,3 +3,57 @@
 // невозможно, программа должна вывести сообщение для
 // пользователя.
 
+Console.Clear();
+
+int[,] GetMatrix(int m, int n)
+{
+    int[,] matrix = new int[m, n];
+    Random rnd = new Random();
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(1, 100);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            if (j == 0) Console.Write("[");
+            if (j < array.GetLength(1) - 1) Console.Write($"{array[i, j],3}, ");
+            else Console.Write($"{array[i, j],3}]");
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] matrixResoult = GetMatrix(3, 3);
+PrintMatrix(matrixResoult);
+
+int[,] ReplacingRowsColumns(int[,] matrixResoult)
+{
+    int[,] matrixNew = new int[matrixResoult.GetLength(0), matrixResoult.GetLength(1)];
+    for (int i = 0; i < matrixResoult.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrixResoult.GetLength(1); j++)
+        {
+            matrixNew[i, j] = matrixResoult[j, i];
+        }
+    }
+    return matrixNew;
+}
+
+Console.WriteLine();
+
+if (matrixResoult.GetLength(0) != matrixResoult.GetLength(1)) Console.WriteLine("Невозможно выполнить");
+else
+{
+    int[,] newMatrix = ReplacingRowsColumns(matrixResoult);
+    PrintMatrix(newMatrix);
+}
