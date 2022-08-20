5ec6f89a46dd98c741411713efce691346c68644
Igor Chichinin <marafon4k@gmail.com>
2022-08-20 19:34:53 +0300

    Added solutions from less live

---
+++ less08/task57/Program.cs
@@ -0,0 +1,95 @@
+﻿// Задача 57: Составить частотный словарь элементов
+// двумерного массива. Частотный словарь содержит
+// информацию о том, сколько раз встречается элемент
+// входных данных.
+
+Console.Clear();
+
+int[,] GetMatrix(int m, int n)
+{
+    int[,] matrix = new int[m, n];
+    Random rnd = new Random();
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(1, 10);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            if (j == 0) Console.Write("[");
+            if (j < array.GetLength(1) - 1) Console.Write($"{array[i, j],3}, ");
+            else Console.Write($"{array[i, j],3}]");
+        }
+        Console.WriteLine();
+    }
+}
+
+int[] array(int[,] array)
+{
+    int[] arrayNew = new int[10];
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            arrayNew[array[i, j]]++;
+        }
+    }
+    return arrayNew;
+}
+
+int[] ConversionArray(int[,] matrix)
+{
+    int[] arrayNew = new int[matrix.Length];
+    int z = 0;
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            arrayNew[z] = matrix[i, j];
+            z++;
+        }
+    }
+    Array.Sort(arrayNew);
+    return arrayNew;
+}
+
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]};  ");
+    }
+    Console.WriteLine();
+}
+
+void WriteDigitCountToConsole(int[] arrayNew)
+{
+    int number = arrayNew[0];
+    int count = 0;
+    for (int i = 0; i < arrayNew.Length; i++)
+    {
+        if (arrayNew[i] == number) count++;
+        else
+        {
+            Console.WriteLine($"Число {number} встречается -> {count} раз");
+            number = arrayNew[i];
+            count = 1;
+        }
+        if(i == arrayNew.Length - 1) Console.WriteLine($"Число {number} встречается -> {count} раз");
+    }
+}
+
+int[,] matrixResoult = GetMatrix(3, 3);
+PrintMatrix(matrixResoult);
+
+int[] arrayNew = ConversionArray(matrixResoult);
+WriteDigitCountToConsole(arrayNew);
---
+++ less08/task59/Program.cs
@@ -0,0 +1,81 @@
+﻿// Задача 59: Задайте двумерный массив из целых чисел.
+// Напишите программу, которая удалит строку и столбец, на
+// пересечении которых расположен наименьший элемент
+// массива.
+
+int[,] GetMatrix(int m, int n)
+{
+    int[,] matrix = new int[m, n];
+    Random rnd = new Random();
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(1, 50);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            if (j == 0) Console.Write("[");
+            if (j < array.GetLength(1) - 1) Console.Write($"{array[i, j],3}, ");
+            else Console.Write($"{array[i, j],3} ]");
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] matrixResoult = GetMatrix(4, 4);
+PrintMatrix(matrixResoult);
+
+int[] FindMinCross(int[,] matrixResult)//Получаем одномерный массив с позицией мин значения
+{
+    int m = 0;
+    int n = 0;
+    int min = matrixResult[0, 0];
+    for (int i = 0; i < matrixResult.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrixResult.GetLength(1); j++)
+        {
+            if (matrixResult[i, j] < min)
+            {
+                min = matrixResult[i, j];
+                m = i;
+                n = j;
+            }
+        }
+    }
+    return new int[] { m, n };
+}
+
+int[] min = FindMinCross(matrixResoult);
+
+int[,] RemoveCross(int[,] matrixResult, int[] findMinCross)
+{
+    int[,] matrix = new int[matrixResult.GetLength(0) - 1, matrixResult.GetLength(1) - 1];
+    int m = 0;
+    int n = 0;
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        if (m == findMinCross[0]) m++;
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (n == findMinCross[1]) n++;
+            matrix[i, j] = matrixResult[m, n];
+            n++;
+        }
+        n = 0;
+        m++;
+    }
+    return matrix;
+}
+
+Console.WriteLine();
+int[,] matrixResoultNew = RemoveCross(matrixResoult, min);
+PrintMatrix(matrixResoultNew);
